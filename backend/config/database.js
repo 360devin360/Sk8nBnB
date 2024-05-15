50db9465c4a51cb3a3eff3eb84cc1b56cccdc016
devin <devin.360@aol.com>
2024-05-14 18:28:17 -0600

    migrated new Users Table

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,25 +1,26 @@
+// backend/config/database.js
 const config = require('./index');
 
 module.exports = {
   development: {
     storage: config.dbFile,
-    dialect:"sqlite",
-    seederStoarage:"sequelize",
+    dialect: "sqlite",
+    seederStorage: "sequelize",
     logQueryParameters: true,
     typeValidation: true
   },
-  production:{
-    use_env_variable:'DATABASE_URL',
-    dialect:'postgres',
-    seederStoarage:"sequelize",
-    dialectOptions:{
-      ssl:{
-        require:true,
-        rejectUnauthorized:false
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
       }
     },
-    define:{
-      schema:process.env.SCHEMA
+    define: {
+      schema: process.env.SCHEMA
     }
   }
-}
+};
---
+++ backend/db/migrations/20240515001524-create-user.js
@@ -0,0 +1,51 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+// define schema name
+// create an option object
+let options = {}
+// check environment (production or development)
+if(process.env.NODE_ENV === 'production'){
+  // if enviorment is production then create schema property in options equal to .env.SCHEMA
+  options.schema = process.env.SCHEMA;
+}
+// add constraints to fields
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING(30),
+        allowNull:false,
+        unique:true
+      },
+      email: {
+        type: Sequelize.STRING(256),
+        allowNull:false,
+        unique:true
+      },
+      hashedPassword: {
+        type: Sequelize.STRING.BINARY,
+        allowNull:false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue:new Date()
+      }
+    },options);
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = "Users";
+    return queryInterface.dropTable(options);
+  }
+};
