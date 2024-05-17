0607a7233e2cf47b768e625a51c1a689fa159e3e
devin <devin.360@aol.com>
2024-05-17 17:43:38 -0600

    added user signup functionality to backend endpoint POST /routes/api/users

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,10 +1,47 @@
 //import required packages
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const {setTokenCookie} = require('../../utils/auth');
+const {User} = require('../../db/models');
 
 //initialize router
 const router = express.Router();
 
-
+// Sign Up Users
+router.post('/', async (req,res,next)=>{
+    // try block
+    try{
+        // deconstruct req.body
+        const {email, password, username} = req.body;
+        // // create a hashed password for user 
+        const hashedPassword = bcrypt.hashSync(password);
+        // // create user record in Users table
+        const user = await User.create({
+            email,
+            username,
+            hashedPassword
+        });
+        // create safeUser object for setTokenCookie function
+        const safeUser = {
+            id:user.id,
+            email:user.email,
+            username:user.username
+        };
+        // set token cookie
+        await setTokenCookie(res,safeUser);
+        // return json of user
+        return res.json({
+            user:safeUser
+        });
+    // forward any errors not already sent
+    }catch(error){
+        error.statusCode = 500
+        next({
+            "message":`${error.message} in POST backend/routes/api/users.js`,
+            status : error.statusCode
+        })
+    };
+});
 
 // export router
 module.exports = router;
