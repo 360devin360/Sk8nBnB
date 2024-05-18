e0dd63d8bb439376963d81c462008717f6da5185
devin <devin.360@aol.com>
2024-05-17 21:47:32 -0600

    add user input validation on signup at backend/routes/api/users.js

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -3,12 +3,37 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const {setTokenCookie} = require('../../utils/auth');
 const {User} = require('../../db/models');
+const {check} = require('express-validator');
+const {handleValidationErrors} = require('../../utils/validation');
 
 //initialize router
 const router = express.Router();
 
+// middleware ------------------------------------------------------
+const validateSignup = [
+    check('email')
+        .exists({checkFalsy:true})
+        .isEmail()
+        .withMessage('Please provide a valid email.'),
+    check('username')
+        .exists({checkFalsy:true})
+        .isLength({min:4})
+        .withMessage('Please provide a username with at least 4 characters.'),
+    check('username')
+        .not()
+        .isEmail()
+        .withMessage('Username cannot be an email'),
+    check('password')
+        .exists({checkFalsy:true})
+        .isLength({min:6})
+        .withMessage('Password must be 6 characters or more.'),
+    handleValidationErrors
+];
+
+// Routes --------------------------------------------------------------------
+
 // Sign Up Users
-router.post('/', async (req,res,next)=>{
+router.post('/', validateSignup, async (req,res,next)=>{
     // try block
     try{
         // deconstruct req.body
