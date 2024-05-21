e4870addd0d9b1133779876e6845301a1e98348b
devin <devin.360@aol.com>
2024-05-21 16:45:31 -0600

    added options to migrations files

--- backend/db/migrations/20240521104611-create-booking.js
+++ backend/db/migrations/20240521104611-create-booking.js
@@ -1,12 +1,12 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
+
 let options = {}
 if(process.env.NODE_ENV === 'production'){
   options.schema = process.env.SCHEMA
 }
 
 module.exports = {
-
   async up(queryInterface, Sequelize) { 
     await queryInterface.createTable('Bookings', {
       id: {
@@ -47,7 +47,7 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue:new Date()
       }
-    });
+    },options);
   },
   async down(queryInterface, Sequelize) {
     options.tableName = 'Bookings'
