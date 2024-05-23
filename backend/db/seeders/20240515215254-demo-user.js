e56cf6a5eab8884e622ea1880a508cbf664597ab
devin <devin.360@aol.com>
2024-05-23 17:30:46 -0600

    added package.json file to root directory, because it was accidentally removed

--- backend/db/seeders/20240515215254-demo-user.js
+++ backend/db/seeders/20240515215254-demo-user.js
@@ -1,4 +1,3 @@
-
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
 
@@ -57,7 +56,7 @@ module.exports = {
      */
     options.tableName = 'Users';
     const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(options,{
+    return await queryInterface.bulkDelete(options,{
       username:{[Op.in]:['Demo-lition','FakeUser1','FakeUser2']}
     },{});
   }
