e71ff956b08bfb66ba8481db62b80f2cb41c2202
devin <devin.360@aol.com>
2024-05-24 03:52:45 -0600

    added table.name

--- backend/db/migrations/20240521091243-create-spot.js
+++ backend/db/migrations/20240521091243-create-spot.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let options = {};
-
+options.tableName = 'Spots'
 if(process.env.NODE_ENV === 'production'){
   options.schema = process.env.SCHEMA
 };
@@ -72,11 +72,11 @@ module.exports = {
       }
     },options);
 
-    // await queryInterface.addIndex(options.tableName='Spots',[
-    //     "address",
-    //     "city",
-    //     "state"
-    //   ])
+    await queryInterface.addIndex(options.tableName,[
+        "address",
+        "city",
+        "state"
+      ]);
   },
 
   async down(queryInterface, _Sequelize) {
