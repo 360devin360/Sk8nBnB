6397a38959b2f487688588ba4aebe4d57eadebc8
devin <devin.360@aol.com>
2024-05-27 03:20:31 -0600

    changing literal

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -41,9 +41,7 @@ router.get('/current',requireAuth,async (req,res,next)=>{
                         select url
                         from SpotImages
                         join Spots
-                        on SpotImages.spotId = Spots.id
-                        and
-                        preview = true)`),'previewImage']
+                        )`),'previewImage']
                 ],
                 raw:true
             },
